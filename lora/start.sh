fe7bfcf0e915e52de59edfed4cc7571421ad31e0
SWoto <brunostinghel137@gmail.com>
2020-10-09 14:09:03 -0300

    Update start.sh
    1. Check for internet connection before running the package forwarder.
    2. Check if the gateway_ID is 0000000000000000 and if so, replace it in the global_conf.json

--- lora/start.sh
+++ lora/start.sh
@@ -12,8 +12,24 @@ sleep 0.1
 echo "0"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
 sleep 0.1
 echo "$SX1301_RESET_BCM_PIN" > /sys/class/gpio/unexport
-./set_eui.sh
-sleep 0.2
-#./update_gwid.sh ./local_conf.json
-sleep 0.5
+
+echo "Checking for internet connectivity..."
+# Test the connection, wait if needed.
+while [[ $(ping -c1 google.com 2>&1 | grep " 0% packet loss") == "" ]]; do
+  echo "Waiting for internet connection..."
+  sleep 30
+  done
+
+grep -q 0000000000000000 global_conf.json
+if [[ $? == 0 ]]
+then
+	echo "First boot config update running"
+    ./set_eui.sh
+    sleep 0.2
+    ./update_gwid.sh ./global_conf.json
+    sleep 0.5
+fi
+
+echo "Starting Packet Forwarder..."
+# Fire up the forwarder.
 ./lora_pkt_fwd
